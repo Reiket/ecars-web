aa1e4c44fec0674c644ec2714b5d59a940b11ba6
Reiket <olinyko2607@gmail.com>
2025-07-10 19:32:03 +0300

    hot fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import dotenv from 'dotenv';
+import {codecovVitePlugin} from '@codecov/vite-plugin';
 
 dotenv.config();
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: 'dist',
+      uploadToken: process.env.CODECOV_TOKEN ?? '',
+    }),
+  ],
   resolve: {
     alias: {
       '@ecars': path.resolve(__dirname, './src/@ecars'),
