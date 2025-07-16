770fac9f1c76bccc9a427853467b0c8d2acef118
Reiket <olinyko2607@gmail.com>
2025-07-16 16:21:40 +0300

    setting up a project

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,15 @@
 import {createBrowserRouter} from 'react-router-dom';
 import {PageUrls} from '@ecars/constants/page-urls';
-import Layout from '@ecars/pages/Layout';
+import Layout from '@ecars/pages/Layout/Layout';
+import {UiKit} from '@ecars/pages/uiKit/UiKit';
 
 export const router = createBrowserRouter([
   {
     path: PageUrls.LAYOUT,
     element: <Layout />,
+  },
+  {
+    path: PageUrls.UIKIT,
+    element: <UiKit />,
   },
 ]);
