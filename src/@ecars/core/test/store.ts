e210935e384c57dc51d7b6d4d01589d177429c06
Reiket <olinyko2607@gmail.com>
2025-11-10 18:01:37 +0200

    fixed eslint for store

--- src/@ecars/core/test/store.ts
+++ src/@ecars/core/test/store.ts
@@ -1,16 +1,23 @@
-import type {EnhancedStore} from '@reduxjs/toolkit';
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+
 import {apiSlice} from '@ecars/core/slices/api/apiSlice';
 import authReducer from '@ecars/core/slices/store/auth/authSlice';
 
-export const setupIntegrationTestStore = (): EnhancedStore => {
+const rootReducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  auth: authReducer,
+});
+
+export type TestRootState = ReturnType<typeof rootReducer>;
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const setupIntegrationTestStore = (preloadedState?: TestRootState) => {
   return configureStore({
-    reducer: {
-      [apiSlice.reducerPath]: apiSlice.reducer,
-      auth: authReducer,
-    },
+    reducer: rootReducer,
+    preloadedState,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   });
 };
 
 export type IntegrationTestStore = ReturnType<typeof setupIntegrationTestStore>;
+export type TestAppDispatch = IntegrationTestStore['dispatch'];
